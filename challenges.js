76969773c50dd57497febb3b21319d639bea83d2
paxluporum <paxluporum@gmail.com>
2025-10-30 10:49:18 -0400

    still going

--- challenges.js
+++ challenges.js
@@ -1,8 +1,9 @@
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d");
 
+import { Star } from "./star.js";
 
-let mySpecialStar = new Star();
+let mySpecialStar = new Star(pencil);
 mySpecialStar.draw();
 
 function gameLoop() {
--- star.js
+++ star.js
@@ -1,14 +1,20 @@
 export class Star {
 
+
     x = 50;
     y = 50;
 
-    draw() {
-        pencil.beginPath();
-        pencil.arc(95, 50, 40, 0, 2 * Math.PI);
-        pencil.fillStyle = "red";
-        pencil.fill();
-        pencil.closePath();
+
+    constructor(pencil) {
+        this.pencil = pencil;
+    }
+
+    draw(pencil) {
+        this.pencil.beginPath();
+        this.pencil.arc(95, 50, 40, 0, 2 * Math.PI);
+        this.pencil.fillStyle = "red";
+        this.pencil.fill();
+        this.pencil.closePath();
 
     }
 
