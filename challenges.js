49aba802720ebc6971ebc7f30cf5f592d0d8be58
paxluporum <paxluporum@gmail.com>
2025-10-30 11:05:56 -0400

    movement

--- challenges.js
+++ challenges.js
@@ -6,15 +6,21 @@ import { Star } from "./star.js";
 let mySpecialStar = new Star(pencil);
 mySpecialStar.draw();
 
-
 let stars = [
     mySpecialStar
 ];
+
 function gameLoop() {
 
+    //erase canvas
+    pencil.clearRect(0, 0, canvas.clientWidth, canvas.height)
     //draw background
     //draw stars
     //move stars
+    for(let i = 0; i < stars.length; i++) {
+        stars[i].move();
+        stars[i].draw();
+    }
     //recycle stars / restart stars
 
 
