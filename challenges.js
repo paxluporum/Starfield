504c4f6138e944654de5a0a05dd6f64979d7b7e2
paxluporum <paxluporum@gmail.com>
2025-10-30 10:40:54 -0400

    working

--- challenges.js
+++ challenges.js
@@ -1,13 +1,17 @@
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d");
 
+
+let mySpecialStar = new Star();
+mySpecialStar.draw();
+
 function gameLoop() {
 
     //draw background
     //draw stars
     //move stars
     //recycle stars / restart stars
-    
+
 
 
 }
